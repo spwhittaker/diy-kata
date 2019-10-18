c2a9a0df4779aa8e722a378dcc780fff1ba3336d
Sam Millward <samuel.j.pearce@hotmail.com>
2019-10-18 19:49:18 +0100

    humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,48 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+let humanAge;
+if (number <= 0) {
+  humanAge = "not born yet";
+}
+if (number < 1 && number > 0) {
+  humanAge = "just a wee un";
+}
+if (number >= 1) {
+  humanAge = number;};
 
+  let catAge;
+  if (number <= 0) {
+    catAge = "not born yet";
+  }
+  if (number < 1 && number > 0) {
+    catAge = "just a wee un";
+  }
+  if (number === 1) {
+    catAge = 15;
+  }
+  if (number === 2) {
+    catAge = 24;
+  }
+  if (number > 2) {
+    catAge = 24 + 4 * (number - 2);
+  }
+
+  let dogAge;
+  if (number <= 0) {
+    dogAge = "not born yet";
+  }
+  if (number < 1 && number > 0) {
+    catAge = "just a wee un";
+  }
+  if (number === 1) {
+    dogAge = 15;
+  }
+  if (number === 2) {
+    dogAge = 24;
+  }
+  if (number > 2) {
+    dogAge = 24 + 5 * (number - 2);
+  }
+  return [humanAge, catAge, dogAge];
+};
 module.exports = humanCatDogYears;
+
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,7 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears" , () => {
+   test("When humanCatDogYears is passed a number (representing human years), it should return an array of three numbers. The first number human years, cat years and dog years respectively", () => {});
+   expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+  expect(humanCatDogYears(5)).toEqual([5, 36, 39]);
+});
