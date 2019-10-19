c808962a77663f09b0186aaf8f2ac2e342e74154
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-19 15:12:38 +0100

    kata7 getEmployeeRole complete

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,8 @@
-const getEmployerRole = (employeeName, employees) => {};
-
+const getEmployerRole = (employeeName, employees) => {
+  let employeeObject = employees.find(e => e.name === employeeName);
+  if (!employeeObject) {
+    return "That person doesn't work here.";
+  }
+  return employeeObject.role;
+};
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,44 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    expect(
+      getEmployerRole("Satti", [
+        {
+          name: "Satti",
+
+          role: "Developer"
+        },
+        {
+          name: "Jenny",
+
+          role: "Sales Associate"
+        },
+        {
+          name: "Javid",
+
+          role: "Human Recommended Reading Assistant"
+        }
+      ])
+    ).toEqual("Developer");
+
+    expect(
+      getEmployerRole("Steven", [
+        {
+          name: "Stephen",
+          role: "Cool Guy"
+        },
+        {
+          name: "Stefan",
+          role: "Cyclist"
+        }
+      ])
+    ).toEqual("That person doesn't work here.");
+    expect(
+      getEmployerRole("sam", [
+        { name: "Sam", role: "Junior Developer" },
+        { name: "SAM", role: "shouter" }
+      ])
+    ).toEqual("That person doesn't work here.");
+  });
 });
