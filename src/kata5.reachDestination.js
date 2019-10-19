caaf6a5cf9927414431b73abf60e948314093c66
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-19 13:55:19 +0100

    completed kata5 reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,27 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    const time = distance / speed;
+  const roundedTime = Math.round(time * 2) / 2;
+
+    if (roundedTime < 0) {
+        return "No negative units, please.";
+    }
+    if (roundedTime === 0) {
+        return "I should be there in less than half an hour";
+    }
+    if (roundedTime === 1) {
+        return "I should be there in 1 hour.";
+    } else {
+        return `I should be there in ${roundedTime} hours.`;
+    }
+};
 
 module.exports = reachDestination;
+
+
+
+/* get exact time as distance in miles divided by mph */
+/* 100 miles at 20mph should be 100 / 20 = 5 */
+/*To get to the nearest half mile, get the number e.g. 4.4, double it to 8.8, round that number to 9, then divide that result by 2 */
+
+// add something so that if it's one hour, it says "I'll be there in 1 hour", rather than hours
+//if it rounds down to less than half an hour (0), "I'll be there in less than half an hour
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,13 @@
-const { reachDestination } = require("../src");
+const {
+  reachDestination
+} = require("../src");
 
 describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {});
+  expect(reachDestination(100, 20)).toBe("I should be there in 5 hours.");
+  expect(reachDestination(10, 10)).toBe("I should be there in 1 hour.");
+  expect(reachDestination(100, 33)).toBe("I should be there in 3 hours.");
+  expect(reachDestination(1, 5)).toBe("I should be there in less than half an hour");
+  expect(reachDestination(10, -10)).toBe("No negative units, please.");
+  expect(reachDestination(5, 0.73)).toBe("I should be there in 7 hours.");
 });
