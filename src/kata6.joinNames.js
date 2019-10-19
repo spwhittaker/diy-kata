099eda66eb1888a02128f056acc2a8d3c31ca4f5
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-19 14:46:15 +0100

    kata6 completed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,23 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  const namesArray = namesObj.map(e => e.name);
+  if (namesArray.length === 1) {
+    return namesArray[0];
+  }
+  const finalName = namesArray.pop();
+  if (namesArray.length >= 2) {
+    const joinedNames = namesArray.join(", ");
+    return `${joinedNames} & ${finalName}`;
+  }
+    return `${namesArray[0]} & ${finalName}`;
+  };
 
 module.exports = joinNames;
+
+/* Ensure not to do an & if there's only one person. 
+Split the array into all the elements apart from the last one, and the last one
+Use pop to remove last name
+only use the join with commas if the array that doesn't include final name has 2 or more elements
+e.g [Bart,Lisa,Maggie] becomes [Bart,Lisa] and "Maggie"
+join the shortened array then add '& final element' at the end
+
+*/
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,58 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, separated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        {
+          name: "Bart"
+        },
+        {
+          name: "Lisa"
+        },
+        {
+          name: "Maggie"
+        }
+      ])
+    ).toEqual("Bart, Lisa & Maggie");
+    expect(
+      joinNames([
+        {
+          name: "Sam"
+        },
+        {
+          name: "Diane"
+        }
+      ])
+    ).toEqual("Sam & Diane");
+    expect(
+      joinNames([
+        {
+          name: "Abed"
+        },
+        {
+          name: "Annie"
+        },
+        {
+          name: "Britta"
+        },
+        {
+          name: "Jeff"
+        },
+        {
+          name: "Pierce"
+        },
+        {
+          name: "Troy"
+        }
+      ])
+    ).toEqual("Abed, Annie, Britta, Jeff, Pierce & Troy");
+    expect(
+      joinNames([
+        {
+          name: "Loner"
+        }
+      ])
+    ).toEqual("Loner");
+  });
 });
