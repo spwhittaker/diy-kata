2a6f51b039b5adaa2790616213be8034c8e0dc68
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-19 15:14:32 +0100

    added comments

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,8 +1,10 @@
 const getEmployerRole = (employeeName, employees) => {
-  let employeeObject = employees.find(e => e.name === employeeName);
+  let employeeObject = employees.find(e => e.name === employeeName); //find the object in the array that has that name value
   if (!employeeObject) {
-    return "That person doesn't work here.";
+    return "That person doesn't work here."; //give a message if there is no object with that name value
   }
-  return employeeObject.role;
+  return employeeObject.role; //return the role value of the object
 };
 module.exports = getEmployerRole;
+
+
