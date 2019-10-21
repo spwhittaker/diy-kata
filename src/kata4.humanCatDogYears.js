884911189ce6c9c9fafa3350eb517156ced37352
Stephen Whittaker <stephenwhittaker23@gmail.com>
2019-10-21 20:31:02 +0100

    reformatted kata4 with switches

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,53 +1,55 @@
 const humanCatDogYears = number => {
   let humanAge;
-  if (number <= 0) {
-    humanAge = "not born yet";
+  switch (true) {
+    case number <= 0:
+      humanAge = "not born yet";
+      break;
+    case number < 1 && number > 0:
+      humanAge = "just a wee un";
+      break;
+    default:
+      humanAge = number;
+      break;
   }
-  if (number < 1 && number > 0) {
-    humanAge = "just a wee un";
-  }
-  if (number >= 1) {
-    humanAge = number;
-  }
-
   let catAge;
-  if (number <= 0) {
-    catAge = "not born yet";
+  switch (true) {
+    case number <= 0:
+      catAge = "not born yet";
+      break;
+    case number < 1 && number > 0:
+      catAge = "just a wee un";
+      break;
+    case number === 1:
+      catAge = 15;
+      break;
+    case number > 1 && number < 2:
+      catAge = 15 + (number - 1) * 9;
+      break;
+    case number === 2:
+      catAge = 24;
+      break;
+    default:
+      catAge = 24 + 4 * (number - 2);
   }
-  if (number < 1 && number > 0) {
-    catAge = "just a wee un";
-  }
-  if (number === 1) {
-    catAge = 15;
-  }
-  if (number > 1 && number < 2) {
-    catAge = 15 + (number - 1) * 9;
-  }
-  if (number === 2) {
-    catAge = 24;
-  }
-  if (number > 2) {
-    catAge = 24 + 4 * (number - 2);
-  }
-
   let dogAge;
-  if (number <= 0) {
-    dogAge = "not born yet";
-  }
-  if (number < 1 && number > 0) {
-    dogAge = "just a wee un";
-  }
-  if (number === 1) {
-    dogAge = 15;
-  }
-  if (number > 1 && number < 2) {
-    dogAge = 15 + (number - 1) * 9;
-  }
-  if (number === 2) {
-    dogAge = 24;
-  }
-  if (number > 2) {
-    dogAge = 24 + 5 * (number - 2);
+  switch (true) {
+    case number <= 0:
+      dogAge = "not born yet";
+      break;
+    case number < 1 && number > 0:
+      dogAge = "just a wee un";
+      break;
+    case number === 1:
+      dogAge = 15;
+      break;
+    case number > 1 && number < 2:
+      dogAge = 15 + (number - 1) * 9;
+      break;
+    case number === 2:
+      dogAge = 24;
+      break;
+    default:
+      dogAge = 24 + 5 * (number - 2);
   }
   return [humanAge, catAge, dogAge];
 };
